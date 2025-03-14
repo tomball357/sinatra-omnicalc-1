0810a05ae9820526305fd3d73f7b4273d92041a7
tomball357 <tball@chicagobooth.edu>
2025-03-14 18:01:43 +0000

    git

--- app.rb
+++ app.rb
@@ -23,10 +23,10 @@ get("/square_root/new") do
 end
 
 get("/square_root/results") do
-  @number = params[:number].to_f
-  @result = &radic(@number).to_f
+  @num = params[:number].to_f
+  @result = Math.sqrt(@number).to_f
   erb :square_root_result
-
+end
 
 get("/payment/new") do
 
@@ -37,16 +37,15 @@ get ("/payment/results") do
   @apr =
   @years =
   @principal =
-
+end
 
 
 get("/random/new") do
 
   erb :random_form
-
+end
 
 get ("/random/results") do
   @result = rand(@min..@max).to_f
-
 
 end

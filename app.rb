c9b0e3727c49e1ddb5742ddf8734c09f46ae06cc
tomball357 <tball@chicagobooth.edu>
2025-03-14 17:48:13 +0000

    save

--- app.rb
+++ app.rb
@@ -1,38 +1,50 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
 get("/") do
-  erb(:homepage)
+  erb(:square_form)
 end
 
 get("/square/new") do
-  erb(:square/new)
+  erb(:square_form)
 end
 
 get "/square/results" do
   @number = params[:number].to_f
-  @result = @number**2
-  erb(:square/results)
+  @result = (@number**@number).to_f
+  erb(:square_result)
 end
 
 
 get("/square_root/new") do
 
-  @square = @num*@num
-
-  erb :square_root
+  erb :square_root_form
 end
+
+get("/square_root/results") do
+  @number = params[:number].to_f
+  @result = &radic(@number).to_f
+  erb :square_root_result
+
 
 get("/payment/new") do
 
   @square = @num*@num
 
-  erb :payment
+  erb :payment_form
 end
+
+get ("/payment/results") do
+  @apr =
+  @years =
+  @principal =
+
+
 
 get("/random/new") do
 
   @new = rand(1..@num)
 
-  erb :random
+  erb :random_form
 end
